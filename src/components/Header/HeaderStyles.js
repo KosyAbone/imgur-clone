808fae4d8cbe0d97317c679ae7dd8deffb2b3070
KOSSY <abonekosiso@gmail.com>
2022-07-16 22:26:15 +0100

    comm

--- src/components/Header/HeaderStyles.js
+++ src/components/Header/HeaderStyles.js
@@ -28,10 +28,10 @@ export const Title = styled.h1`
     font-weight: 700;
     letter-spacing: 1px;
     
-    span{
+    ${'' /* span{
         color: #52d978;
     }
-`
+` */}
 export const Button = styled.button`
 
 `
