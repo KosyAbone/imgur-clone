678ac74933ea3d298507b59c5081392ddc3ff588
KOSSY <abonekosiso@gmail.com>
2022-07-20 23:36:30 +0100

    com

--- src/components/Styles/GlobalStyles.js
+++ src/components/Styles/GlobalStyles.js
@@ -8,9 +8,9 @@ export const GlobalStyles = createGlobalStyle`
     margin: 0;
     padding: 0;
   }
-  body {
+  ${'' /* body {
     font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, Helvetica, Arial, sans-serif, "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol";
     height: 100vh;
     text-rendering: optimizeLegibility;
   }
-  `
+  ` */}
