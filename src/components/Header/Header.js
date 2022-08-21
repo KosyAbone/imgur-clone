40bf8cb9805e88f1aa4f1380e5dd528714b8d485
KOSSY <abonekosiso@gmail.com>
2022-08-21 23:55:39 +0100

    adjust

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -3,18 +3,18 @@ import { StyledHeader, NavBar, MenuButton, Title, Button } from './HeaderStyles'
 
 function Header() {
   return (
-    <StyledHeader>
-      <NavBar>
-        <MenuButton> 
-          <div></div>
-          <div></div>
-          <div></div>
-        </MenuButton>
-        <Title><span>S</span>naps</Title>
-        <Button>Sign in</Button>
-        <Button>Sign up</Button>
-      </NavBar>
-    </StyledHeader>
+    // <StyledHeader>
+    //   <NavBar>
+    //     <MenuButton> 
+    //       <div></div>
+    //       <div></div>
+    //       <div></div>
+    //     </MenuButton>
+    //     <Title><span>S</span>naps</Title>
+    //     <Button>Sign in</Button>
+    //     <Button>Sign up</Button>
+    //   </NavBar>
+    // </StyledHeader>
   )
 }
 
